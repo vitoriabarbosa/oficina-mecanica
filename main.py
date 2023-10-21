978775dc01a33f5a72a9fc5338e5fd4c4741e9fc
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2023-10-21 15:50:04 -0300

    adicionando cores

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ class Car:
         self.country = country
         self.typeCar = typeCar
 
-
     def cadastro(self):
         print("Cor:", self.color)
         print("Marca:", self.brand)
@@ -16,10 +15,9 @@ class Car:
         print("País de fabricação:", self.country)
         print("Tipo do carro:", self.typeCar)
 
-
     def servico(self):
         print("\n\nNOSSOS SERVIÇOS")
-        print("\n>> Promoção! Qualquer serviço por R$ 80,00! <<")
+        print("\n\033[92m>> Promoção! Qualquer serviço por R$ 80,00! <<\033[0m\033[96m")
         return "\n[ 1 ] - Encher Pneu \n[ 2 ] - Trocar Pneu \n[ 3 ] - Trocar Peças \n[ 4 ] - Consertos \n[ 5 ] - Reparos Gerais\n"
 
     def encherPneu(self):
@@ -51,7 +49,7 @@ class Car:
         else:
             return "\nOpção não identificada...  :( \n"
 
-print(f'{"-"*80} \n{"SEJAM BEM-VINDOS A OFICINA RUBY!!".center(60, " ")} \n{"-"*80}')
+print(f'\033[1;95m{"-"*80}\n\n{"SEJAM BEM-VINDOS A OFICINA RUBY!!".center(80, " ")}\n\n{"-"*80}\033[0m')
 print("\nREALIZE O CADASTRO DO SEU CARRO: \n")
 
 cor = input("Cor: ")
@@ -63,6 +61,6 @@ tipo = input("Tipo do seu carro: ")
 carro = Car(cor, marca, preco, modelo, pais, tipo)
 
 print(carro.servico())
-conserto = int(input("DIGITE O NÚMERO DO SERVIÇO: "))
+conserto = int(input("\033[0mDIGITE O NÚMERO DO SERVIÇO: "))
 print(carro.conserto())
-print("\nVOLTE SEMPRE! :)")
+print("\nVOLTE SEMPRE! :) ")
